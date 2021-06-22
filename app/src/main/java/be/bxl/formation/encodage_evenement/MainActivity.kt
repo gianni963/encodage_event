b10b615add98d6534273644f9330108930d43479
gianni963 <gianni_guatieri@hotmail.com>
2021-06-22 08:04:49 -0700

    correction adapter

--- app/src/main/java/be/bxl/formation/encodage_evenement/MainActivity.kt
+++ app/src/main/java/be/bxl/formation/encodage_evenement/MainActivity.kt
@@ -9,9 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        todoAdapter = EventAdapter(mutableListOf())
+
+        //adapter
+        eventAdapter = EventAdapter(mutableListOf())
         rvEventItems.adapter = EventAdapter
         tvEventItems.layoutManager = LinearLayoutManager(this)
+
+        rvEventItems.adapter = todoAdapter
+        rvTEventItems.layoutManager = LinearLayoutManager(this)
 
         btnAddEvent.setOnClickListener{
             var eventTitle = etEventTitle.text.toString()
